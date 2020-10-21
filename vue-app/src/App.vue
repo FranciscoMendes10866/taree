23c371fd3eca421d76327afc4b6582b7e7212b79
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-10-21 23:11:08 +0200

    init

---
+++ vue-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Navbar from "./components/Navbar.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Navbar
+  }
+});
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap');
+* {
+  font-family: 'Titillium Web', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
